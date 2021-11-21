ff2939de0e907f53185b61c272eaf583999cf17c
susana <susir95@gmail.com>
2021-11-21 18:49:11 -0500

    adding filters form to watch in mobile and pc view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Main } from './components/main/Main';
+import { Header } from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
     </div>
   );
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Cards } from '../cards/Cards';
 import { Filters } from '../filters/Filters';
-import { Header } from '../header/Header';
 import './styles.css';
 
 export const Main = () => {
   return (
     <main className="main">
-      <Header />
       <Filters />
       <Cards />
     </main>
